86475add77d3c046c073e99d39daa3a901b38298
My1758 <134392467+My1758@users.noreply.github.com>
2023-05-24 15:59:06 +0300

    Update Main.java

--- src/main/java/efs.task.oop/Main.java
+++ src/main/java/efs.task.oop/Main.java
@@ -1,15 +1,94 @@
 package efs.task.oop;
 
-class Villager {
+interface Fighter {
+    void attack(Fighter victim);
+    void takeHit(int damage);
+    boolean isDead();
+}
+
+abstract class Monster implements Fighter {
+    protected int health;
+    protected int damage;
+    protected boolean isDead;
+
+    public Monster(int health, int damage) {
+        this.health = health;
+        this.damage = damage;
+        this.isDead=false;
+    }
+    public void setHealth(int health){
+        this.health=health;
+    }
+    public void setDamage(int damage){
+        this.damage=damage;
+    }
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+    public boolean isDead(){
+        return isDead;
+    }
+}
+
+class Monsters {
+    static final Monster andariel = new Monster(10, 70) {
+        @Override
+        public void attack(Fighter victim) {
+            victim.takeHit(damage);
+        }
+
+        @Override
+        public void takeHit(int damage) {
+            if (!isDead) {
+                health -= damage;
+                Monsters.monstersHealth -= damage;
+                if (health <= 0 && monstersHealth <= 0) {
+                    health = 0;
+                    monstersHealth=0;
+                    isDead = true;
+                }
+            }
+        }
+    };
+
+    static final Monster blacksmith = new Monster(100, 25) {
+        @Override
+        public void attack(Fighter victim) {
+            victim.takeHit(damage);
+        }
+
+        @Override
+        public void takeHit(int damage) {
+            health -= (5 + damage);
+            Monsters.monstersHealth -= (5 + damage);
+        }
+    };
+
+    static int monstersHealth = andariel.getHealth() + blacksmith.getHealth();
+}
+
+
+class Villager implements Fighter {
     private String name;
     private int age;
+    private int health;
+    private boolean isDead;
 
     public Villager(String name, int age) {
         this.name = name;
         this.age = age;
+        this.health=100;
+        this.isDead=false;
     }
     public void setName(String name){
         this.name=name;
+    }
+    public boolean isDead(){
+        return isDead;
     }
     public String getName(){
         return name;
@@ -20,7 +99,24 @@ class Villager {
     public int getAge(){
         return age;
     }
+    public int getHealth(){
+        return health;
+    }
+    public void attack(Fighter victim) {
+        int damage = (int) ((100 - age * 0.5) / 10);
+        victim.takeHit(damage);
+    }
 
+    @Override
+    public void takeHit(int damage) {
+        if (!isDead) {
+            health -= damage;
+            if (health <= 0) {
+                health = 0;
+                isDead = true;
+            }
+        }
+    }
     public void sayHello() {
         System.out.println("Greetings traveler... I'm " + name + " and I'm " + age + " years old.");
     }
@@ -43,11 +139,26 @@ class ExtraordinaryVillager  extends Villager{
     }
 
     private Skill skill;
-
+    private boolean isDead;
     public ExtraordinaryVillager(String name, int age, Skill skill) {
         super(name, age);
         this.skill = skill;
+        this.isDead=false;
+        
     }
+    public void attack(Fighter victim) {
+        // ExtraordinaryVillagers don't attack
+    }
+
+    @Override
+    public void takeHit(int damage) {
+        // ExtraordinaryVillagers die from one hit
+        super.takeHit(damage);
+        if (getHealth() <= 0) {
+            isDead = true;
+        }
+    }
+    
 
     @Override
     public void sayHello() {
@@ -74,6 +185,9 @@ public class Main {
        deckardCain.sayHello();
         villager5.sayHello();
         villager6.sayHello();
+
+        
     }
 }
+
 
