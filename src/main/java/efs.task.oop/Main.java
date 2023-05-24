ef396eee733cbd588f7dfa6c34f6811c0921d6da
My1758 <134392467+My1758@users.noreply.github.com>
2023-05-24 14:14:00 +0300

    Update Main.java

--- src/main/java/efs.task.oop/Main.java
+++ src/main/java/efs.task.oop/Main.java
@@ -11,7 +11,7 @@ class Villager {
     public void setName(String name){
         this.name=name;
     }
-    public String setName(){
+    public String getName(){
         return name;
     }
     public void setAge(int age){
@@ -26,23 +26,54 @@ class Villager {
     }
 }
 
+class ExtraordinaryVillager  extends Villager{
+    public enum Skill {
+        IDENTIFY("I will identify items for you at no charge."),
+        SHELTER("I can offer you poor shelter.");
+
+        private String description;
+
+        Skill(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
+
+    private Skill skill;
+
+    public ExtraordinaryVillager(String name, int age, Skill skill) {
+        super(name, age);
+        this.skill = skill;
+    }
+
+    @Override
+    public void sayHello() {
+        System.out.println("Greetings traveler... I'm " + getName() + " and I'm " + getAge() + " years old. " + skill.getDescription());
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Villager villager1 = new Villager("Kashya", 30);
-        Villager villager2 = new Villager("Akara", 40);
+        //Villager villager2 = new Villager("Akara", 40);
+        ExtraordinaryVillager akara = new ExtraordinaryVillager("Akara", 40, ExtraordinaryVillager.Skill.SHELTER);
         Villager villager3 = new Villager("Gheed", 50);
-        Villager villager4 = new Villager("Deckard Cain", 85);
+        //Villager villager4 = new Villager("Deckard Cain", 85);
+        ExtraordinaryVillager deckardCain = new ExtraordinaryVillager("Deckard Cain", 85, ExtraordinaryVillager.Skill.IDENTIFY);
         Villager villager5 = new Villager("Warriv", 35);
         Villager villager6 = new Villager("Flawia", 25);
 
         villager1.sayHello();
-        villager2.sayHello();
+       // villager2.sayHello();
+       akara.sayHello();
         villager3.sayHello();
-        villager4.sayHello();
+       // villager4.sayHello();
+       deckardCain.sayHello();
         villager5.sayHello();
         villager6.sayHello();
     }
 }
-
-
 
